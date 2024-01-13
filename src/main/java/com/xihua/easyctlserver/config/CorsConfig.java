91028b7f11c45af0f8274a4d8d01b86b83232f51
xihua <xihuaxone@outlook.com>
2024-01-14 00:14:17 +0800

    web  merge;

--- src/main/java/com/xihua/easyctlserver/config/CorsConfig.java
+++ src/main/java/com/xihua/easyctlserver/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
